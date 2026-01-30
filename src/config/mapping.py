71491ef2fa2e42f75d35e7d379fc57e7f937d636
ABDELLAH EL BOUCHOUARI <abdo.elbouchouari@gmail.com>
2026-01-30 20:45:58 +0100

    setting mapping config file

---
+++ src/config/mapping.py
@@ -0,0 +1,36 @@
+SUBJECT_MAPPING = {
+    "الرياضيات": {
+        "label_en": "mathematics",
+        "label_fr": "mathématiques",
+        "language": "ar" #Pour d'assurer que le LLM respecte le contexte pédagogique du matière qui est en arabe
+    },
+    "Français": {
+        "label_en": "french",
+        "label_fr": "français",
+        "language": "fr"
+    },
+    "الاجتماعيات": {
+        "label_en": "social_studies",
+        "label_fr": "histoire_geographie",
+        "language": "ar"
+    },
+    "التربية الاسلامية": {
+        "label_en": "islamic_education",
+        "label_fr": "education_islamique",
+        "language": "ar"
+    },
+    "اللغة العربية": {
+        "label_en": "arabic_language",
+        "label_fr": "langue_arabe",
+        "language": "ar"
+    },
+    "النشاط العلمي": {
+        "label_en": "science",
+        "label_fr": "sciences",
+        "language": "ar"
+    }
+}
+
+GRADE_MAPPING = {
+    "السادس ابتدائي": "6th_grade"
+}
